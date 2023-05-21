d3b3aab77f29ceaceb43fcb0f401dc8dcb4f0dea
Yan Gidorini <ygidorini@gmail.com>
2023-05-20 21:41:56 -0300

    Initial commit

---
+++ back-end-api/src/main/java/com/netshoes/api/WebscrapperNetshoesApplication.java
@@ -0,0 +1,18 @@
+package com.netshoes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class WebscrapperNetshoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebscrapperNetshoesApplication.class, args);
+
+		log.info("SERVIDOR INICIADO");
+	}
+
+}
---
+++ back-end-api/src/test/java/com/webscrapper/netshoes/webscrapper_netshoes/WebscrapperNetshoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webscrapper.netshoes.webscrapper_netshoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebscrapperNetshoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
