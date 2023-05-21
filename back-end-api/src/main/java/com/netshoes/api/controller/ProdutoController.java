d539fe507429d8698d965bdf9303d69839bd38b7
Yan Gidorini <ygidorini@gmail.com>
2023-05-21 15:19:41 -0300

    feat: update CORS policy

--- back-end-api/src/main/java/com/netshoes/api/controller/ProdutoController.java
+++ back-end-api/src/main/java/com/netshoes/api/controller/ProdutoController.java
@@ -3,6 +3,7 @@ package com.netshoes.api.controller;
 import java.io.IOException;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/produto")
+@CrossOrigin(origins = "*")
 public class ProdutoController {
 
     private ProdutoService service;
